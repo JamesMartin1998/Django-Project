c5b8a8fb67c84fbfd649e02238f829f733bdd520
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-10-13 14:22:54 +0000

    add view for generating the movie list

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Movies
 
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+
+class MoviesList(generic.ListView):
+    model = Movies
+    template_name = 'movies.html'
