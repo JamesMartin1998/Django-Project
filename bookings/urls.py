5d2f8a7e75da4b63e93f2d79f46968d239b6f839
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-10-13 17:30:02 +0000

    remove one url and view for the movies page to fix the error where database data wasn't loading into the movies.html

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ShowPage.home, name='home'),
-    path('movies/', views.ShowPage.movies, name='movies'),
+    # path('movies/', views.ShowPage.movies, name='movies'),
+    path('movies/', views.get_movies, name='movies'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from .models import Movies
+from django.template import loader
 
 # Create your views here.
 
@@ -10,14 +11,13 @@ class ShowPage():
     def home(request):
         return render(request, 'index.html')
 
-    def movies(request):
-        return render(request, 'movies.html')
+    # def movies(request):
+    #     return render(request, 'movies.html')
 
 
-
-
-
-
-class MoviesList(generic.ListView):
-    model = Movies
-    template_name = 'movies.html'
+def get_movies(request):
+    movies = Movies.objects.all()
+    context = {
+        'movies': movies
+    }
+    return render(request, 'movies.html', context)
